be564793ddc2b8e55c9c9a1b92d235d7ef018daa
motch-mo <youthful_days@icloud.com>
2020-05-09 10:16:56 +0900

    deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,8 +36,8 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set: rbenv_type, :user
-set: rbenv_ruby, '2.5.1'
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
